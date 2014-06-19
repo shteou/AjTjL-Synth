1f755484c188871b235267eca862670155d1fb63
Stewart Platt <stew@futurete.ch>
2014-06-19 23:08:34 +0100

    Fixed a bug with matrix order (width/height inverted)

--- public/js/synth/grid.js
+++ public/js/synth/grid.js
@@ -17,9 +17,9 @@ define('synth/grid',
 
     return {
       init: function() {
-        for(var i=0; i<_gridHeight; ++i) {
+        for(var i=0; i<_gridWidth; ++i) {
           _grid.push([]);
-          for(var j=0; j<_gridWidth; ++j) {
+          for(var j=0; j<_gridHeight; ++j) {
             _grid[i].push({
               type: "empty",
               note: null
@@ -29,10 +29,12 @@ define('synth/grid',
       },
 
       getTileByPosition: function(x, y) {
+        // TODO: Fix me, boundary case
         return getTile(Math.floor(x / 10), Math.floor(y / 10));
       },
 
       setTileByPosition: function(x, y, tile) {
+        // TODO: Fix me, boundary case
         setTile(Math.floor(x / 10), Math.floor(y / 10), tile);
       }
     }
