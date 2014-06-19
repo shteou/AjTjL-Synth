9162520b6759a1f2f343a9a5c9b9256a31d32413
Stewart Platt <stew@futurete.ch>
2014-06-19 21:27:15 +0100

    Added some utility methods to the grid. Set width/height in terms of tiles.

--- public/js/synth/grid.js
+++ public/js/synth/grid.js
@@ -2,16 +2,38 @@ define('synth/grid',
   ['synth/tiles'],
   function(Tile) {
     var _grid = [],
-        _gridLength = 100;
+        _width = 800,
+        _height = 600,
+        _gridWidth = _width / 10,
+        _gridHeight = _height / 10;
 
     return {
       init: function() {
-        for(var i=0; i<_gridLength; ++i) {
+        for(var i=0; i<_gridHeight; ++i) {
           _grid.push([]);
-          for(var j=0; j<_gridLength; ++j) {
-            _grid[i].push(new Tile.Empty());
+          for(var j=0; j<_gridWidth; ++j) {
+            _grid[i].push({
+              type: "empty",
+              note: null
+            });
           }
         }
+      },
+
+      getTile: function(x, y) {
+        return _grid[x][y];
+      },
+
+      setTile: function(x, y, tile) {
+        _grid[x][y] = tile;
+      },
+
+      getTileByPosition: function(x, y) {
+        return getTile(Math.floor(x / 10), Math.floor(y / 10));
+      },
+
+      setTileByPosition: function(x, y, tile) {
+        setTile(Math.floor(x / 10), Math.floor(y / 10), tile);
       }
     }
 })
