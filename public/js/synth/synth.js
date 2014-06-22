7dba9931633ef5777d6080e6bc4685e729dcf651
stew <stew@futurete.ch>
2014-06-22 06:49:34 +0400

    Increased tempo and changed mouse actions to onDown/onDrag

--- public/js/synth/synth.js
+++ public/js/synth/synth.js
@@ -2,11 +2,27 @@ define('synth/synth',
   ['synth/utility', 'synth/grid', 'synth/instruments', 'synth/tiles', 'synth/notes'],
   function(Utility, Grid, Instruments, Tiles, Notes) {
     var _goo,
-        _tempo = 60;
+        _tempo = 120;
 
     function play() {
       setInterval(Instruments.advance, (60/_tempo)*1000);
     }
+
+    function onMouse(goo) {
+      if(Instruments.getCurrentInstrument() && Instruments.getCurrentInstrument() !== '') {
+        Instruments.setInstrumentLocation(Math.floor(goo.mouseX / Grid.getTileSize()),
+          Math.floor(goo.mouseY / Grid.getTileSize()));
+        Instruments.clearCurrentInstrument();
+     } else {
+      var tile = {
+        note: Notes.getNote(),
+      	tileType: Tiles.getTileType() === 'erase' ? 'empty' : Tiles.getTileType()
+       };
+
+       Grid.setTileByPosition(goo.mouseX, goo.mouseY, tile);
+     }
+   }
+
 
     return {
       init: function() {
@@ -29,20 +45,8 @@ define('synth/synth',
 
       	  	// Update + Render the instruments
       	  },
-      	  onMouseUp: function(goo) {
-      	    if(Instruments.getCurrentInstrument() && Instruments.getCurrentInstrument() !== '') {
-              Instruments.setInstrumentLocation(Math.floor(goo.mouseX / Grid.getTileSize()),
-                Math.floor(goo.mouseY / Grid.getTileSize()));
-              Instruments.clearCurrentInstrument();
-      	    } else {
-      	      var tile = {
-      	            note: Notes.getNote(),
-      	            tileType: Tiles.getTileType() === 'erase' ? 'empty' : Tiles.getTileType()
-      	      };
-
-	      Grid.setTileByPosition(goo.mouseX, goo.mouseY, tile);
-      	    }
-          }
+      	  onMouseDrag: onMouse,
+          onMouseDown: onMouse
       	});
       }
     }
