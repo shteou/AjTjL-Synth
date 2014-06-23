085792c27b3299f2170f136467e2178baddb933d
stew <stew@futurete.ch>
2014-06-23 10:41:52 +0400

    Implement server-side saving.

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
 // Required modules
-var express = require('express');
-var cors = require('cors');
+var express = require('express'),
+    cors = require('cors'),
+    fs = require('fs'),
+    bodyParser = require('body-parser');
 
 // Create our simple express app
 var app = express();
 
 // Setup all middleware
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use(express.static(process.cwd() + '/public'));
+
+app.post('/save', function(req, res) {
+  console.log(req.body);
+  fs.writeFile('save/' + req.body.id, JSON.stringify(req.body), function(err) {
+    if(err) {
+      res.send(500, "Error " + err);
+    } else {
+      res.send("Success");
+    }
+  });
+});
 
 // Create a HTTP server on port 3000
 var server = app.listen(3333, function() {
